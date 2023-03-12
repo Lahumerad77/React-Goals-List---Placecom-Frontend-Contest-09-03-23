50b9521b38585ee2ce90bbdd10b6090983b71da6
Lahumerad77 <93986046+Lahumerad77@users.noreply.github.com>
2023-03-12 15:37:06 +0530

    Add files via upload

---
+++ src/components/ListOfGoals.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ListOfGoals = ({ goals }) => (
+  <ul>
+    {goals.map(({ goal, by }, i) => (
+      <li key={i}>
+        My goal is to {goal}, by {by}.
+      </li>
+    ))}
+  </ul>
+);
+
+export default ListOfGoals;
